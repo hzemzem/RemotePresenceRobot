7a94a1b5ebd09fa29ff20c815725c45bc57da6fa
hzemzem <hajar.zemzem@gmail.com>
2022-01-28 16:43:25 -0500

    saving

---
+++ src/layouts/videoChatPage.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+function VideoChatPage() {
+    return (
+        <div className="video-chat-page container">
+            <div className="video-chat-window">
+
+            </div>
+            <div className="sidebar">
+                
+            </div>
+        </div>
+    );
+}
+
+export default VideoChatPage;
