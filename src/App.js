9f2bb0d79f8bf00e73506a5f8aa9815a7a9cd70e
hzemzem <hajar.zemzem@gmail.com>
2022-04-04 12:04:30 -0400

    pushing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import Video from './components/video'
+import Video from './components/video';
+import RobotVideo from './components/robotVideo';
 import './App.css';
-import './styles/video.css'
+import './styles/video.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { goToRoomInput } from './components/goToRoomInput';
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
        <React.Fragment>
           <Route path="/" exact component={goToRoomInput}/>
           <Route path="/:roomId" exact component={Video}/>
+          <Route path="/robot/rpr9cs" exact component={RobotVideo}/>
         </React.Fragment>
       </BrowserRouter>
     )
