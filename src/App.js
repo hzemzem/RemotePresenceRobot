4f80f9c4abec7f5601769a339c20e304b4ecba99
hzemzem <hajar.zemzem@gmail.com>
2022-04-22 16:09:29 -0400

    robot-side-app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import Video from './components/video'
 import './App.css';
@@ -16,23 +15,6 @@ class App extends Component {
       </BrowserRouter>
     )
   }
-=======
-import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './layouts/Login';
-import VideoChatPage from './layouts/VideoChatPage';
-import React, { useState } from 'react';
-
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login/>} />
-                <Route path="/video-chat-page" element={<VideoChatPage/>} />
-            </Routes>
-        </BrowserRouter>
-    );
->>>>>>> dev
 }
 
 export default App;
